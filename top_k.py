7b255480350987eb85505f5c1e43a84aed33abe2
danielG <greenspan1993@gmail.com>
2018-12-01 19:58:02 +0200

    top_k

--- top_k.py
+++ top_k.py
@@ -11,7 +11,7 @@ fd.close()
 
 
 def dis(w1,w2):
-    d = np.max([float(la.norm(w1, 2) * la.norm(w2, 2)), 1e-8])
+    d = np.max([float(np.linalg.norm(w1, 2) * np.linalg.norm(w2, 2)), 1e-8])
     n = np.dot(u, v)
     return n / d
 

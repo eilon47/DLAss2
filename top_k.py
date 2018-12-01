8be8412562677101e7c8dca4f960ed4d49c21528
Eilon <eilon47@gmail.com>
2018-12-01 20:46:53 +0200

    top k

--- top_k.py
+++ top_k.py
@@ -1,47 +1,26 @@
 import numpy as np
-import tagger1_utils as utils
 from tagger1_utils import WORD_TO_VEC as W2V
-
-word_vec = "wordVectors.txt"
-vocab = "vocab.txt"
-vecs = np.loadtxt(word_vec)
-fd = open(vocab,'r')
-data = fd.readlines()
-fd.close()
 
 
 def dis(w1,w2):
-
     d = np.max([float(np.linalg.norm(w1, 2) * np.linalg.norm(w2, 2)), 1e-8])
     n = np.dot(w1, w2)
     return n / d
 
-    #vec1 = W2V[w1]
-    #vec2 = W2V[w2]
-    #p1 = np.dot(vec1, vec2)
-    #p2 = np.sqrt(np.dot(vec1, vec1))
-    #p3 = np.sqrt(np.dot(vec2, vec2))
-    #distance = np.divide(p1,np.multiply(p2,p3))
-    #return distance
-
 
 def most_similar(word, k=5):
     similarities = []
-    most_similar = []
     for w in W2V.keys():
         if w is word:
             continue
-        distance = dis(W2V[word],W2V[w])
-        similarities.append((w, distance))
+        similarities.append((w, dis(W2V[word],W2V[w])))
+    top_k = sorted(similarities, key=lambda x: x[1], reverse=True)
 
-    for i in range(k):
-
-        min_dis = min(similarities.items(), key=lambda x: x[1])
-        most_similar.append(min_dis)
-
-    return similarities
+    return top_k[0:k]
 
 
 if __name__ == '__main__':
-    most_similar('dog')
+    for w in ["dog", "england", "john","explode", "office"]:
+        for t in most_similar(w):
+            print t
 
